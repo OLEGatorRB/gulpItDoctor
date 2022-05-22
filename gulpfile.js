eecec08602333ba14d2119c8832e88501d4d3d25
OLEGatorRB <olegatorrb@gmail.com>
2022-05-22 10:28:08 +0300

    добавил gulp-less, gulp-sass, gulp-rename, gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,36 @@
 const gulp = require('gulp');
-const less = require('less');
-const scss = require('scss');
+const less = require('gulp-less');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css')
 const del = require('del');
+
+const paths = {
+	styles: {
+		src: 'src/styles/**/*.scss',
+		dest: 'dist/css'
+	},
+	scripts: {
+		src: 'src/scripts/**/*.js',
+		dest: 'dist/js'
+	}
+};
 
 function clean() {
 	return del(['dist']);
 }
 
-exports.clean = clean;
+function styles() {
+	return gulp.src(paths.styles.src)
+		.pipe(less())
+		.pipe(sass())
+		.pipe(cleanCSS())
+		.pipe(rename({
+			basename: 'main',
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(paths.styles.dest))
+}
+
+exports.clean = clean
+exports.styles = styles;
